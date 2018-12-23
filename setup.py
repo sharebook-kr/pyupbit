0cbd61b127e6b68d0d6ed01559aff3faa460645d
jonghun.yoo <jonghun.yoo@outlook.com>
2018-12-23 11:12:50 +0900

    1) requirtements.txt 추가 2) versino 0.0.2로 변경

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name            = 'pyupbit',
-    version         = '0.1.0',
+    version         = '0.2.0',
     description     = 'python wrapper for Upbit API',
     url             = 'https://github.com/sharebook-kr/pyupbit',
     author          = 'Lukas Yoo, Brayden Jo',
     author_email    = 'jonghun.yoo@outlook.com, pystock@outlook.com',
-    install_requires=['requests'],
+    install_requires=['requests', 'pandas', 'jwt'],
     license         = 'MIT',
     packages        = ['pyupbit'],
     zip_safe        = False
