72b2dafde480e12ff44dc559411aa535f354f1ce
Brayden Jo <brayden.jo@outlook.com>
2019-06-02 12:46:26 +0900

    version up

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name            = 'pyupbit',
-    version         = '0.2.3',
+    version         = '0.2.4',
     description     = 'python wrapper for Upbit API',
     url             = 'https://github.com/sharebook-kr/pyupbit',
     author          = 'Lukas Yoo, Brayden Jo',
-    author_email    = 'hyunho.jo@outlook.com, jonghun.yoo@outlook.com, pystock@outlook.com',
+    author_email    = 'brayden.jo@outlook.com, jonghun.yoo@outlook.com, pystock@outlook.com',
     install_requires= ['requests', 'pandas', 'pyjwt'],
     license         = 'MIT',
     packages        = ['pyupbit'],
