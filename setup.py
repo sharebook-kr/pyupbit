1a8f5b8df03b702f723f26a6d303ef7988dc5bb7
Jinsung Ha <jsung5381@naver.com>
2020-12-18 21:54:29 +0900

    Implement get_spec function
    이 get_spec 함수를 이용함으로써, requirements.txt 에 업데이트 된
    사항들을 자동적으로 반영할 수 있습니다.
    원래 구현되어있던 setup.py는 requirements.txt에 있는 사항들을 전부
    반영하지 않아, "python setup.py install" 커맨드가 실패하는
    현상이 존재했습니다. 이를 해결하기 위하여 get_spec 함수를
    구현하였습니다.

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
+import io
+import os
+
 from setuptools import setup
 
+
+def get_spec(filename, mode='r'):
+    def wrapper():
+        here = os.path.dirname(__file__)
+        result = {}
+        with io.open(os.path.join(here, filename), encoding='utf-8') as src_file:
+            result = src_file.read()
+        return result
+    return wrapper
+
+
+get_requirements = get_spec('requirements.txt')
 setup(
     name            = 'pyupbit',
     version         = '0.2.6',
@@ -7,7 +22,7 @@ setup(
     url             = 'https://github.com/sharebook-kr/pyupbit',
     author          = 'Lukas Yoo, Brayden Jo',
     author_email    = 'brayden.jo@outlook.com, jonghun.yoo@outlook.com, pystock@outlook.com',
-    install_requires= ['requests', 'pandas', 'pyjwt'],
+    install_requires= get_requirements(),
     license         = 'MIT',
     packages        = ['pyupbit'],
     zip_safe        = False
