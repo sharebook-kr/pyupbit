2fda7b913484d16a64fcdf31ecaf5c7f4ab94db1
Brayden Jo <brayden.jo@outlook.com>
2021-05-23 14:11:33 +0900

    remove md parser

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,6 @@ release = '0.3.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_parser"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
